acc64eaa122f5a05a5a805b7ec20a21c26a4a13f
Дмитрий <sa-well-ev@yandex.ru>
2018-05-03 13:22:37 +0700

    Урок 7 Вывод товаров из соответствующей категориии!
    Signed-off-by: Дмитрий <sa-well-ev@yandex.ru>

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -18,4 +18,12 @@ class CategoryController extends AppController
         $hits = Product::find()->where(['hit' => '1'])->limit(6)->all();
         return $this->render('index', compact('hits'));
     }
+
+    public function actionView($id)
+    {
+        //Непонятно зачем переписывать переменную $id когда она и так приходит из вне в параметре метода
+        //$id = Yii::$app->request->get('id');
+        $products = Product::find()->where(['category_id' => $id])-> all();
+        return $this->render('view', compact('products'));
+    }
 }
