73283511afb6f06b238525ab6e1ef2c1e018e2a9
Дмитрий <sa-well-ev@yandex.ru>
2018-05-08 09:43:31 +0300

    Урок 20 Оформление заказа часть 2 Реализовано сохранение заказа в таблицы 1. подправили количетво столбцов в таблице 2. взяли количество и сумму из сессии в ордер 3. Используя поведение заносим даты создание и изменения заказа в ордер. через тригеры таблицы 4. Сохраняем запись о заказе в ордер. 5. В вид заказа добавили из прошлого курса флэш сообщения в начало разметки. 6. После сохранения заказа в таблицу ордер забираем его айди, и вызываем saveOrderItems - сохраняем детализацию заказа 7. Пишем метод saveOrderItems который в цикле проходит массив сессии и сохраняет его в таблицу ордеритемс 8. Очистим корзину после сохранения заказа

--- views/cart/view.php
+++ views/cart/view.php
@@ -4,6 +4,19 @@ use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 ?>
 <div class="container">
+    <?php if( Yii::$app->session->hasFlash('success') ): ?>
+        <div class="alert alert-success alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('success'); ?>
+        </div>
+    <?php endif;?>
+
+    <?php if( Yii::$app->session->hasFlash('error') ): ?>
+        <div class="alert alert-danger alert-dismissible" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <?php echo Yii::$app->session->getFlash('error'); ?>
+        </div>
+    <?php endif;?>
     <?php if(!empty($session['cart'])): ?>
         <div class="table-responsive">
             <table class="table table-hover table-striped">
@@ -30,11 +43,11 @@ use yii\widgets\ActiveForm;
                 <?php endforeach?>
                 <tr>
                     <td colspan="4">Итого: </td>
-                    <td><?= $session['cart.qty']?></td>
+                    <td colspan="2"><?= $session['cart.qty']?></td>
                 </tr>
                 <tr>
                     <td colspan="4">На сумму: </td>
-                    <td><?= $session['cart.sum']?></td>
+                    <td colspan="2"><?= $session['cart.sum']?></td>
                 </tr>
                 </tbody>
             </table>
