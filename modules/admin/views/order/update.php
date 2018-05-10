1f98b17231a693f92aa3eb86aea483fc77ba0016
Дмитрий <sa-well-ev@yandex.ru>
2018-05-10 12:11:21 +0300

    25. Управление заказами ч.3 Обновление заказа 1. вид Update орусифицировали 2. вид _form - поменяли выпадающий список

--- modules/admin/views/order/update.php
+++ modules/admin/views/order/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Order */
 
-$this->title = "Обновить заказ №: {$model->id}";
+$this->title = "Редактирование заказа №: {$model->id}";
 $this->params['breadcrumbs'][] = ['label' => 'Заказы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
