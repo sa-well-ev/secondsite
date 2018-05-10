28873073b956bf23fd5233f78c6fc287b98be1c6
Дмитрий <sa-well-ev@yandex.ru>
2018-05-10 11:16:31 +0300

    25. Управление заказами ч.1 1. Делаем через gii модель modules\admin\models\OrderItems.php

--- modules/admin/views/order/update.php
+++ modules/admin/views/order/update.php
@@ -5,8 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Order */
 
-$this->title = 'Update Order: {nameAttribute}';
-$this->params['breadcrumbs'][] = ['label' => 'Orders', 'url' => ['index']];
+$this->title = "Обновить заказ №: {$model->id}";
+$this->params['breadcrumbs'][] = ['label' => 'Заказы', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
